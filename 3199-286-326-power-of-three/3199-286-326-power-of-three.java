d6ab93fde6a0e51dba34d7336be07113cf266f93
VEDANT MOHOL <152087070+vedantmohol@users.noreply.github.com>
2025-02-26 20:03:16 +0530

    Time: 8 ms, Memory: 44 MB - LeetHub

---
+++ 3199-286-326-power-of-three/3199-286-326-power-of-three.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        if(n>1){
+            return (n % 3 == 0) && isPowerOfThree(n/3);
+        }
+        else{
+            return false;
+        }
+    }
+}
