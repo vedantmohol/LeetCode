bce879baa543ace85eb6c145471030d8df847bdf
VEDANT MOHOL <152087070+vedantmohol@users.noreply.github.com>
2025-04-30 06:42:23 +0530

    Time: 1 ms, Memory: 42.4 MB - LeetHub

---
+++ 0092-0-3340-check-balanced-string/0092-0-3340-check-balanced-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int even=0,odd=0;
+
+        for(int i=0 ; i<num.length() ; i++){
+            if(i%2==0){
+               even += (num.charAt(i) -'0');
+            }else{
+               odd += (num.charAt(i) - '0');
+            }
+        }   
+        return (even==odd);
+    }
+}
