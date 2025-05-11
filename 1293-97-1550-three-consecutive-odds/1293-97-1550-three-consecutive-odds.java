14140469ac688ef170f6257ea9c3b71e5721a0d7
VEDANT MOHOL <152087070+vedantmohol@users.noreply.github.com>
2025-05-11 19:07:50 +0530

    Time: 0 ms, Memory: 42.2 MB - LeetHub

---
+++ 1293-97-1550-three-consecutive-odds/1293-97-1550-three-consecutive-odds.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        if(arr.length<3){
+            return false;
+        }
+        for(int i=0 ; i<arr.length-2 ; i++){
+            if(arr[i]%2 != 0){
+                if(arr[i+1]%2!=0 && arr[i+2]%2!=0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
